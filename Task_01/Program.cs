8d1f8783ffdb28f1300e6a08272fa9b35a56e518
ekaterina <katerina_w@icloud.com>
2022-06-19 22:15:49 +0300

    add task_1

---
+++ Task_01/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число"); 
+int NumberN = int.Parse(Console.ReadLine()); 
+
+Console.WriteLine("Введите второе число");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Число: " + numberM);
+Console.WriteLine("Квадрат числа: " + numberM * numberM);
+
+if (NumberN == numberM * numberM)
+{
+    Console.WriteLine("УРА, первое число является квадратом второго!");
+    }
+    
+else 
+{
+Console.WriteLine("Первое число не является квадратом второго!");
+}
