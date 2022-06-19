d7323b82c56d14af939054d262e54a1c503fb586
ekaterina <katerina_w@icloud.com>
2022-06-19 22:50:31 +0300

    add task_02

---
+++ Task_02/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая будет выдавать название 
+//дня недели по заданному номеру.
+Console.WriteLine ("Введите номер дня недели от 1 до 7");
+
+int WeekDayNo = int.Parse(Console.ReadLine());
+ 
+ if (WeekDayNo ==1)
+ {
+    Console.WriteLine("Понедельник");
+ }
+ else if (WeekDayNo == 2)
+ {
+    Console.WriteLine("Вторник");
+ }
+ else if (WeekDayNo == 3)
+ {
+    Console.WriteLine("Среда");
+ }
+ else if (WeekDayNo == 4)
+ {
+    Console.WriteLine("Четверг");
+ }
+ else if (WeekDayNo == 5)
+ {
+    Console.WriteLine("Пятница");
+ }
+ else if (WeekDayNo == 6)
+ {
+    Console.WriteLine("Суббота");
+ }
+ else if (WeekDayNo == 7)
+ {
+    Console.WriteLine("Воскресенье");
+ }
+ else
+ {
+    Console.WriteLine("error");
+ }
